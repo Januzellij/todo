bea406124ce728e00e444be88f477754f2be2a75
Jake Januzelli <jake_boaconstrictor@yahoo.com>
2013-11-16 16:12:12 -0500

    cleanup and comments

--- todo.js
+++ todo.js
@@ -1,7 +1,9 @@
+// new mongodb collection to store todos in
 Todos = new Meteor.Collection('todos');
 
 if (Meteor.isClient) {
   Template.allTodos.todos = function () {
+    // return all todos of the current user in alphabetical order
     return Todos.find({user: Meteor.user()}, {sort: {name: 1}});
   }
 
@@ -10,15 +12,18 @@ if (Meteor.isClient) {
   }
 
   Template.todo.tagged = function () {
+    // if the session tag is in the todos tags, it will return True
     return this.tags.indexOf(Session.get('tag')) > -1; 
   }
 
   Template.todocontent.addingtag = function () {
+    // returns true if this is the todo that is getting a tag added
     return Session.get('addingtag') === this.name;
   }
 
   Template.todocontent.tags = function () {
     var todo_id = this._id;
+    // return an array with each object (a tag) having 2 attributes, the id of the todo it belongs to, and the tag itself.
     return _.map(this.tags || [], function (tag) {
       return {todo_id: todo_id, tag: tag};
     });
@@ -26,31 +31,34 @@ if (Meteor.isClient) {
 
   Template.forms.events({
     'click #search': function () {
+      // set a session variable with the tag to search
       Session.set('tag', $('#tag').val());
     },
 
     'click #add': function () {
+      // add a todo with the contents of the todo text box, and the current user as the user
       Todos.insert({name: $('#todo').val(), user: Meteor.user()});
     }
   });
 
   Template.todocontent.events({
     'click #tagbtn': function () {
+      // set a session variable with the name of the todo that is having a tag added
       Session.set('addingtag', this.name);
     },
 
     'blur #tagtext': function () {
+      // adds the contents of the tag text box as a new tag to the current todo
       Todos.update({_id: this._id}, {$push: {tags: $('#tagtext').val()}});
-      console.log(this._id);
     },
 
     'click .tag': function () {
+      // removes the tag that was clicked on from the todo
       Todos.update({_id: this.todo_id}, {$pull: {tags: this.tag}});
-    }
-  });
+    },
 
-  Template.todo.events({
-  	'click .name': function () {
+    'click .name': function () {
+      // removes the todo that was clicked on
       Todos.remove({_id: this._id});
     }
   });
